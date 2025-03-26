e8c02518dd4996f5314b8fe91eac7aab22abd3a1
timofeykafanov <tim.kafanov@yandex.ru>
2025-03-26 18:07:38 +0100

    add ex05

---
+++ cpp01/ex05/main.cpp
@@ -0,0 +1,22 @@
+#include "Harl.hpp"
+
+int main() {
+    Harl harl;
+
+    std::cout << "Testing DEBUG complaint:" << std::endl;
+    harl.complain("DEBUG");
+    
+    std::cout << "\nTesting INFO complaint:" << std::endl;
+    harl.complain("INFO");
+    
+    std::cout << "\nTesting WARNING complaint:" << std::endl;
+    harl.complain("WARNING");
+    
+    std::cout << "\nTesting ERROR complaint:" << std::endl;
+    harl.complain("ERROR");
+    
+    std::cout << "\nTesting invalid complaint:" << std::endl;
+    harl.complain("INVALID");
+
+    return 0;
+}
